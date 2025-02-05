6ae64cfd376410416a9a1ed769a5ae131df3dab5
Mengdi Wang <mengdi.wang@gatech.edu>
2025-02-05 00:50:44 -0500

    reuse src for cirrus and tests

--- xmake.lua
+++ xmake.lua
@@ -2,39 +2,25 @@ add_rules("mode.debug", "mode.release", "mode.releasedbg")
 set_languages("c++17")
 
 includes("./common/xmake.lua")
-
-add_requires("eigen >=3.4.0")
-add_requires("cuda", {system = true})
-add_requires("vtk >=9.3.1", {configs = {cuda = true}})
-add_requires("polyscope =2.3")
-add_requireconfs("polyscope.imgui", {override = true, version = "1.91.1"})
+includes("./src/xmake.lua")
 
 set_rundir("$(projectdir)")
 
 target("cirrus")
     set_kind("binary")
-    add_headerfiles("src/*.h", "ext/*.h")
-    add_files("src/*.cpp", "src/*.cu","main.cpp")
-    add_includedirs("src", "ext", {public = true})
+    add_files("main.cpp")
     if is_plat("windows") then
         set_values("build.vcxproj.includes", "$(CUDA_PATH)/include")
     end
-
     add_cugencodes("native")
     add_cuflags("-extended-lambda --std=c++17 -lineinfo")
-
-    add_packages("cuda", {public = true})
-    add_packages("eigen", {public = true})
-    add_packages("vtk", {public = true})
-    add_packages("polyscope", {public = true})
-
-    add_deps("common")
+    add_deps("common","src")
 
 target("tests")
     set_kind("binary")
-    add_headerfiles("src/*.h", "ext/*.h", "tests/*.h")
-    add_files("src/*.cpp", "src/*.cu","tests/*.cpp", "tests/*.cu")
-    add_includedirs("src", "ext", "tests", {public = true})
+    add_headerfiles("tests/*.h")
+    add_files("tests/*.cpp", "tests/*.cu")
+    add_includedirs("tests", {public = true})
     if is_plat("windows") then
         set_values("build.vcxproj.includes", "$(CUDA_PATH)/include")
     end
@@ -42,9 +28,4 @@ target("tests")
     add_cugencodes("native")
     add_cuflags("-extended-lambda --std=c++17 -lineinfo")
 
-    add_packages("cuda", {public = true})
-    add_packages("eigen", {public = true})
-    add_packages("vtk", {public = true})
-    add_packages("polyscope", {public = true})
-
-    add_deps("common")
+    add_deps("common","src")
