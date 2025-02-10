61660b131aadc5583aed890c12214d5e8fbc3d42
Mengdi Wang <mengdi.wang@gatech.edu>
2025-02-09 20:59:30 -0500

    test amg convergence

--- tests/main.cpp
+++ tests/main.cpp
@@ -16,9 +16,9 @@ int main(int argc, char** argv) {
 		TestGrids::twosources67,
 		}) {
 
-		SolverTests::TestAMGLaplacianAndFluxConsistency(grid_name);
+		//SolverTests::TestAMGLaplacianAndFluxConsistency(grid_name);
 		//SolverTests::TestNeumannDirichletRecovery(grid_name, "cmg");
-		//SolverTests::TestNeumannDirichletRecovery(grid_name, "amg");
+		SolverTests::TestNeumannDirichletRecovery(grid_name, "amg");
 	}
 	return 0;
 }
