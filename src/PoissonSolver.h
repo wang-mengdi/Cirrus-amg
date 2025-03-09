46974bf21909dbc2799df528fa533e0d8b69af7d
Mengdi Wang <mengdi.wang@gatech.edu>
2025-03-09 17:11:08 -0400

    remove everything from poissonsolver

--- src/PoissonSolver.h
+++ src/PoissonSolver.h
@@ -3,8 +3,5 @@
 #include "PoissonGrid.h"
 #include <Eigen/Dense>
 
-//coarsening for Multigrid
-//calculate NONLEAF and GHOST cell types from 
-void CalcCellTypesFromLeafs(HADeviceGrid<Tile>& grid);
 
-void ReCenterLeafCells(HADeviceGrid<Tile>& grid, const int channel, DeviceReducer<double>& cnt_reducer, double* d_mean, double* d_count);
+
