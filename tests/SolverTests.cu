678f39b42bac4d8c5c77fca73335dd8cd46e930a
yuchen-sun-eecs <1800012703@pku.edu.cn>
2025-03-12 14:05:27 -0400

    add sdf function for sphere and star

--- tests/SolverTests.cu
+++ tests/SolverTests.cu
@@ -526,12 +526,16 @@ namespace SolverTests {
     };
     class SphereSolid05GridCase {
     public:
+        __hostdev__ static T phi(const Vec& pos){
+			const Vec ctr(0.5, 0.5, 0.5);
+			constexpr T radius = 0.5 / 2; 
+			return (pos - ctr).length() - radius;
+        }
+
         __hostdev__ static int target(const HATileAccessor<Tile>& acc, HATileInfo<Tile>& info, const int min_level, const int max_level) {
             auto bbox = acc.tileBBox(info);
             auto bmin = bbox.min();
             auto bmax = bbox.max();
-            const Vec ctr(0.5, 0.5, 0.5);
-            constexpr T radius = 0.5 / 2;
             int inside_cnt = 0;
             for (int di : { 0, 1 }) {
                 for (int dj : { 0, 1 }) {
@@ -540,7 +544,7 @@ namespace SolverTests {
                         vpos[0] = bmin[0] + di * (bmax[0] - bmin[0]);
                         vpos[1] = bmin[1] + dj * (bmax[1] - bmin[1]);
                         vpos[2] = bmin[2] + dk * (bmax[2] - bmin[2]);
-                        if ((vpos - ctr).length() < radius) {
+                        if (phi(vpos) < 0) {
                             inside_cnt++;
                         }
                     }
@@ -553,8 +557,6 @@ namespace SolverTests {
 			auto pos = acc.cellCenter(info, l_ijk);
             auto dx = acc.voxelSize(info);
 			Vec bmin(pos[0] - 0.5 * dx, pos[1] - 0.5 * dx, pos[2] - 0.5 * dx);
-            const Vec ctr(0.5, 0.5, 0.5);
-            constexpr T radius = 0.5 / 2;
             int inside_cnt = 0;
             for (int di : { 0, 1 }) {
                 for (int dj : { 0, 1 }) {
@@ -563,7 +565,7 @@ namespace SolverTests {
                         vpos[0] = bmin[0] + di * dx;
                         vpos[1] = bmin[1] + dj * dx;
                         vpos[2] = bmin[2] + dk * dx;
-                        if ((vpos - ctr).length() < radius) {
+                        if (phi(vpos) < 0) {
                             inside_cnt++;
                         }
                     }
@@ -648,6 +650,17 @@ namespace SolverTests {
 
     class StarSolidGerrisGridCase {
     public:
+        __hostdev__ static T phi(const Vec& pos) {
+            T x = pos[0] - 0.5;
+            T y = pos[1] - 0.5;
+            T z = pos[2] - 0.5;
+            T r = sqrt(x * x + y * y + z * z);
+            T theta = acos(z / r);
+            T phi = atan2(y, x);
+
+            T r0 = 0.237 + 0.079 * cos(6 * theta) * cos(6 * phi);
+            return r - r0;
+        }
         __hostdev__ static int target(const HATileAccessor<Tile>& acc, HATileInfo<Tile>& info, const int min_level, const int max_level) {
             auto bbox = acc.tileBBox(info);
             auto bmin = bbox.min();
@@ -661,18 +674,7 @@ namespace SolverTests {
                         vpos[1] = bmin[1] + dj * (bmax[1] - bmin[1]);
                         vpos[2] = bmin[2] + dk * (bmax[2] - bmin[2]);
 
-                        T x = vpos[0] - 0.5;
-                        T y = vpos[1] - 0.5;
-                        T z = vpos[2] - 0.5;
-                        T r = sqrt(x * x + y * y + z * z);
-                        T theta = acos(z / r);
-                        T phi = atan2(y, x);
-
-                        T r0 = 0.237 + 0.079 * cos(6 * theta) * cos(6 * phi);
-
-                        //T r0 = 0.237 + 0.079 * cos(6 * theta);
-
-                        if (r < r0) {
+                        if (phi(vpos) < 0) {
                             inside_cnt++;
                         }
                     }
@@ -697,18 +699,7 @@ namespace SolverTests {
                         vpos[1] = bmin[1] + dj * dx;
                         vpos[2] = bmin[2] + dk * dx;
 
-                        T x = vpos[0] - 0.5;
-                        T y = vpos[1] - 0.5;
-                        T z = vpos[2] - 0.5;
-                        T r = sqrt(x * x + y * y + z * z);
-                        T theta = acos(z / r);
-                        T phi = atan2(y, x);
-
-                        T r0 = 0.237 + 0.079 * cos(6 * theta) * cos(6 * phi);
-
-                        //T r0 = 0.237 + 0.079 * cos(6 * theta);
-
-                        if (r < r0) {
+                        if (phi(vpos) < 0) {
                             inside_cnt++;
                         }
                     }
