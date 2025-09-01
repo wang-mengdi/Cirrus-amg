4c14d0c30f208e3aee0900adc5a86b263fa66ce3
Mengdi Wang <mengdi.wang@gatech.edu>
2025-09-01 04:30:42 -0400

    try to remove karman

--- impulse/FluidParams.cu
+++ impulse/FluidParams.cu
@@ -2,40 +2,7 @@
 
 
 __device__ uint8_t FluidParams::cellType(const T current_time, const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk, int& boundary_axis, int& boundary_off) const {
-	if (mTestCase == KARMAN) {
-		//x+ air
-		//other walls
-		bool is_neumann = false;
-		bool is_dirichlet = false;
-
-
-
-		//int boundary_axis, boundary_off;
-		if (queryBoundaryDirection(acc, info, l_ijk, boundary_axis, boundary_off)) {
-			is_neumann = true;
-		}
-
-		const Vec center(0.25, 0.5, 0.5);
-		const T radius = 0.15;
-		auto pos = acc.cellCenter(info, l_ijk);
-
-		//if ((pos - center).length() <= radius) {
-		//	return NEUMANN;
-		//}
-
-		if (0.25 <= pos[1] && pos[1] <= 0.75) {
-			pos[1] = center[1];
-			if ((pos - center).length() <= radius) {
-				return NEUMANN;
-			}
-		}
-
-
-		if (is_neumann) return NEUMANN;
-		else if (is_dirichlet) return DIRICHLET;
-		else return INTERIOR;
-	}
-	else if (mTestCase == SMOKESPHERE) {
+	if (mTestCase == SMOKESPHERE) {
 		//z+ air
 		//other walls
 		bool is_neumann = false;
@@ -63,17 +30,7 @@ __device__ uint8_t FluidParams::cellType(const T current_time, const HATileAcces
 __device__ void FluidParams::setInitialCondition(HATileAccessor<Tile>& acc, HATileInfo<Tile>& info, const Coord& l_ijk) const {
 	double current_time = 0.0;
 
-	if (mTestCase == KARMAN) {
-		Vec initial_vel = Vec(karman_source, 0, 0);
-
-		Tile& tile = info.tile();
-		for (int axis : {0, 1, 2}) {
-			tile(AdvChnls::u + axis, l_ijk) = initial_vel[axis];
-		}
-		int boundary_axis, boundary_off;
-		tile.type(l_ijk) = cellType(current_time, acc, info, l_ijk, boundary_axis, boundary_off);
-	}
-	else if (mTestCase == SMOKESPHERE) {
+	if (mTestCase == SMOKESPHERE) {
 		Vec initial_vel = smokesphere_source;
 
 		Tile& tile = info.tile();
--- impulse/FluidParams.h
+++ impulse/FluidParams.h
@@ -53,7 +53,8 @@ namespace OutputChnls {
 
 
 //TVORTEX: tornado-like vortex, reference: Physically-based Simulation of Tornadoes
-enum TestCase { KARMAN = 0, SMOKESPHERE };
+//enum TestCase { KARMAN = 0, SMOKESPHERE };
+enum TestCase { SMOKESPHERE = 0 };
 
 class FluidParams {
 public:
@@ -65,28 +66,30 @@ public:
 	T mRefineThreshold;
 	T mParticleLife;
 	nanovdb::Vec3R mGravity;
-	MaskGridAccessor mMaskGridAccessor;
-	SDFGridAccessor mSDFGridAccessor;
 
-	SDFGridAccessor mSDFVelocityAccessors[3];
 
-	//for karman
-	T karman_source = 1.0;
+	//MaskGridAccessor mMaskGridAccessor;
+	//SDFGridAccessor mSDFGridAccessor;
 
-	//Vec smokesphere_source = Vec(1.0, 0., 0.);
-	Vec smokesphere_source = Vec(0., 0., 0.4);
-	Vec smokesphere_center = Vec(0.5, 0.5, 0.3);
-	//Vec smokesphere_center = Vec(0.5, 0.5, 0.7);
-	T smokesphere_radius = 0.05;
+	//SDFGridAccessor mSDFVelocityAccessors[3];
 
-	T nasa_source = 1.0;
+	////for karman
+	//T karman_source = 1.0;
 
-	T prop_source = 1.0;
-	T f1_source = 1.0;
-	T lizard_source = +1.0;
-	T fish_source = 1.0;
-	T bat_source = 1.0;
-	T flamingo_source = 1.0;
+	////Vec smokesphere_source = Vec(1.0, 0., 0.);
+	//Vec smokesphere_source = Vec(0., 0., 0.4);
+	//Vec smokesphere_center = Vec(0.5, 0.5, 0.3);
+	////Vec smokesphere_center = Vec(0.5, 0.5, 0.7);
+	//T smokesphere_radius = 0.05;
+
+	//T nasa_source = 1.0;
+
+	//T prop_source = 1.0;
+	//T f1_source = 1.0;
+	//T lizard_source = +1.0;
+	//T fish_source = 1.0;
+	//T bat_source = 1.0;
+	//T flamingo_source = 1.0;
 
 	//parameters set by itself
 	bool mIsPureNeumann = false;
@@ -95,11 +98,10 @@ public:
 	FluidParams(json& j, std::shared_ptr<MaskGrid> mask_grid_ptr, std::shared_ptr<SDFGrid> sdf_float_grid_ptr) {
 
 		std::string test = Json::Value<std::string>(j, "test", "tvortex");
-		if (test == "karman") mTestCase = KARMAN;
-		else if (test == "smokesphere") mTestCase = SMOKESPHERE;
+		if (test == "smokesphere") mTestCase = SMOKESPHERE;
 		else Assert(false, "invalid test {}", test);
 
-		if (mTestCase == KARMAN || mTestCase == SMOKESPHERE) {
+		if (mTestCase == SMOKESPHERE) {
 			mIsPureNeumann = true;
 		}
 
@@ -110,38 +112,6 @@ public:
 		mGravity = Vec(0, 0, 0);
 		mGravity[2] = Json::Value<double>(j, "gravity", -9.8);
 		mParticleLife = Json::Value<T>(j, "particle_life", FLT_MAX);
-
-		if (mask_grid_ptr) {
-			mMaskGridAccessor = mask_grid_ptr->GetDeviceAccessor();
-		}
-		if (sdf_float_grid_ptr) {
-			mSDFGridAccessor = sdf_float_grid_ptr->GetDeviceAccessor();
-		}
-	}
-
-	__device__ static bool queryBoundaryDirection(const HATileAccessor<Tile>& acc, const int level, const Coord& g_ijk, int& boundary_axis, int& boundary_off) {
-		boundary_axis = -1;
-		boundary_off = 0;
-
-		for (int axis : {0, 1, 2}) {
-			for (int off : {-1, 1, 2}) {
-				auto ng_ijk = g_ijk; ng_ijk[axis] += off;
-				HATileInfo<Tile> ninfo; Coord nl_ijk;
-				acc.findVoxel(level, ng_ijk, ninfo, nl_ijk);
-				if (ninfo.empty()) {
-					boundary_axis = axis;
-					boundary_off = off;
-					return true;
-				}
-
-			}
-		}
-		return false;
-	}
-
-	__device__ static bool queryBoundaryDirection(const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk, int& boundary_axis, int& boundary_off) {
-		auto g_ijk = acc.localToGlobalCoord(info, l_ijk);
-		return queryBoundaryDirection(acc, info.mLevel, g_ijk, boundary_axis, boundary_off);
 	}
 
 	__device__ static bool queryBoundaryDirection1(const HATileAccessor<Tile>& acc, const int level, const Coord& g_ijk, int& boundary_axis, int& boundary_off) {
@@ -181,175 +151,14 @@ public:
 		return mCoarseLevel;
 	}
 
-	__hostdev__ bool isInParticleGenerationRegion(const T current_time, const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const Coord& l_ijk)const {
-		if (mTestCase == KARMAN) {
-			auto pos = acc.cellCenter(info, l_ijk);
-			
-
-
-			//return true;
-			const Vec center(0.25, 0.5, 0.5);
-			T radius = 0.175;
-
-			//return (0.5-radius <= pos[2] && pos[2] <= 0.5+radius);
-
-			//radius = 0.35;
-
-
-			if (0.25 <= pos[1] && pos[1] <= 0.75) {
-				pos[1] = center[1];
-				if ((pos - center).length() <= radius) {
-					return true;
-				}
-			}
-			return false;
-		}
-		else if (mTestCase == SMOKESPHERE) {
-			auto pos = acc.cellCenter(info, l_ijk);
-			T gen_radius = smokesphere_radius + 0.025;
-			if ((pos - smokesphere_center).length() <= gen_radius) {
-				return true;
-			}
-			return false;
-		}
-		return false;
-	}
-
 	__device__ uint8_t cellType(const T current_time, const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk, int& boundary_axis, int& boundary_off) const;
 
 
-	//Physically-based Simulation of Tornadoes, Xiangyang Ding
-	__device__ Vec boundaryVelocityDing(const Vec& pos, const int axis, const int sgn) const {
-		if (axis == 2) {//bottom/top boundary
-			if (sgn == -1) {
-				//zero for bottom
-				return Vec(0, 0, 0);
-			}
-			else {
-				T r = sqrt(0.05);
-				auto x = pos[0];
-				auto y = pos[1];
 
-				auto rx2 = ((x - 0.5) / r) * ((x - 0.5) / r);
-				auto ry2 = ((y - 0.5) / r) * ((y - 0.5) / r);
-				T w = 2 * exp(-(rx2 + ry2));
-				return Vec(0, 0, w);
-			}
-		}
-		else {
-			auto z = pos[2];
-			//auto shear_scale = 2 * (1.0 - z);//bottom magnitude 2, top 0
-			//auto shear_scale = (z >= 0.5) ? 1.0 : z * 2;
-			auto shear_scale = 2;
-			Vec v = Vec(1, 1, 0) * shear_scale;
-			if ((axis == 0 && sgn == 1) || (axis == 1 && sgn == 1)) v[0] *= -1;
-			if ((axis == 0 && sgn == -1) || (axis == 1 && sgn == 1)) v[1] *= -1;
-			return v;
-		}
-	}
-
-	__device__ Vec boundaryVelocityForceVortex(const Vec& pos, const int axis, const int sgn)const {
-		//return boundaryVelocityDing(pos, axis, sgn);
-		if (axis == 2) {//bottom/top boundary
-			return Vec(0, 0, 0);
-		}
-		else {
-			auto z = pos[2];
-			//auto shear_scale = 2 * (1.0 - z) * 2;//bottom magnitude 2, top 0
-			//auto shear_scale = 2 * z * 2;//bottom magnitude 0, top 2
-			auto shear_scale = 2 * 1 * 2;
-			Vec r = pos - Vec(0.5, 0.5, 0);
-			r[2] = 0;
-			Vec omega(0, 0, shear_scale);
-			return omega.cross(r);
-		}
-	}
-
-	//boundary axis, sgn indicates which face it is on relative to the whole computational domain
-	//axis indicates the component to be set
-	__device__ void setSlipBoundary(const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk, const int boundary_axis, const int boundary_off, const int axis) const {
-		auto& tile = info.tile();
-		//suppose boundary_axis=1, sgn=-1, l_ijk=(i,0,k), l1_ijk=(i,1,k)
-		Coord l1_ijk = l_ijk; l1_ijk[boundary_axis] -= boundary_off;
-		if (axis != boundary_axis) {
-			tile(AdvChnls::u + axis, l_ijk) = tile(AdvChnls::u + axis, l1_ijk);
-		}
-		//otherwise there is a velocity node right on the face, should be either inflow/outflow or DIRICHLET
-	}
-	//non-slip with given velocity vector
-	//vel_func(pos, boundary_axis, boundary_sgn)
-	template<class FuncVII>
-	__device__ void setFlowBoundary(const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk, const int boundary_axis, const int boundary_off, const FuncVII& vel_func, const int axis) const {
-		auto& tile = info.tile();
-		//suppose boundary_axis=1, sgn=-1, l_ijk=(i,0,k), l1_ijk=(i,1,k)
-		//Coord l1_ijk = l_ijk; l1_ijk[boundary_axis] -= 1 * boundary_sgn;
-
-		if (axis == boundary_axis) {
-			if (boundary_off == -1) {
-				//set face centers 0,1
-
-				//we will not cross the tile so it's ok to use local coord
-				auto l1_ijk = l_ijk; l1_ijk[boundary_axis] += 1;
-				auto pos = acc.faceCenter(axis, info, l1_ijk);
-				auto vel = vel_func(pos, boundary_axis, boundary_off);
-				tile(AdvChnls::u + axis, l1_ijk) = vel[axis];
-
-				Coord l2_ijk = l_ijk; l2_ijk[boundary_axis] += 2;
-				tile(AdvChnls::u + axis, l_ijk) = 2 * vel[axis] - tile(AdvChnls::u + axis, l2_ijk);
-			}
-			else if (boundary_off == 1) {
-				//auto pos = acc.faceCenter(axis, info, l_ijk);
-				//auto vel = vel_func(pos, boundary_axis, boundary_sgn);
-				//tile(AdvChnls::u + axis, l_ijk) = vel[axis];
-			}
-			else if (boundary_off == 2) {
-				//set cells n-1, n-2
-				//this cell is n-2
-
-				auto pos = acc.faceCenter(axis, info, l_ijk);
-				auto vel = vel_func(pos, boundary_axis, boundary_off);
-				tile(AdvChnls::u + axis, l_ijk) = vel[axis];
-
-				//cell n-3
-				Coord l3_ijk = l_ijk; l3_ijk[boundary_axis] -= 1;
-				Coord l1_ijk = l_ijk; l1_ijk[boundary_axis] += 1;
-				tile(AdvChnls::u + axis, l1_ijk) = 2 * vel[axis] - tile(AdvChnls::u + axis, l3_ijk);
-			}
-		}
-		else {
-			Coord l1_ijk = l_ijk; l1_ijk[boundary_axis] -= boundary_off;
-			auto pos0 = acc.faceCenter(axis, info, l_ijk);
-			auto pos1 = acc.faceCenter(axis, info, l1_ijk);
-			auto pos = (pos0 + pos1) * 0.5;
-			auto vel = vel_func(pos, boundary_axis, boundary_off);
-			tile(AdvChnls::u + axis, l_ijk) = 2 * vel[axis] - tile(AdvChnls::u + axis, l1_ijk);
-		}
-	}
-
-
-
-	//__device__ Vec freeVortexLambOseenVelocity()
 
 	__device__ void setBoundaryCondition(const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk, const double current_time) const {
 		auto& tile = info.tile();
-		if (mTestCase == KARMAN) {
-			auto source_vel = karman_source;
-			auto inflow_vel_func = [=]__device__(const Vec & pos, const int axis, const int sgn) {
-				return Vec(source_vel, 0, 0);
-			};
-			int boundary_axis, boundary_sgn;
-			if (queryBoundaryDirection(acc, info, l_ijk, boundary_axis, boundary_sgn)) {
-				for (int axis : {0, 1, 2}) {
-					if (axis != boundary_axis) {
-						setSlipBoundary(acc, info, l_ijk, boundary_axis, boundary_sgn, axis);
-					}
-					else if (tile.type(l_ijk) == NEUMANN) {
-						setFlowBoundary(acc, info, l_ijk, boundary_axis, boundary_sgn, inflow_vel_func, axis);
-					}
-				}
-			}
-		}
-		else if (mTestCase == SMOKESPHERE) {
+		if (mTestCase == SMOKESPHERE) {
 			Vec vel = smokesphere_source;
 			if (tile.type(l_ijk) & NEUMANN) {
 				int boundary_axis, boundary_sgn;
@@ -373,38 +182,6 @@ public:
 		}
 	}
 
-	//gamma: vortex strength
-	//radius: radius of the vortex ring
-	//delta: thickness of the vortex ring
-	//center: center of the vortex ring
-	//unit_x: unit vector of vortex in the x direction
-	//unit_y: unit vector of vortex in the y direction
-	//num_samples: number of sample points on the vortex ring
-	//reference: https://github.com/zjw49246/particle-flow-maps/blob/main/3D/init_conditions.py
-	__device__ void addVortexRingInitialVelocityAndSmoke(const Vec& pos, double gamma, float radius, float delta, const Vec& center, const Vec& unit_x, const Vec& unit_y, int num_samples, Vec& velocity, T& smoke) const {
-
-
-		// Curve length per sample point
-		float curve_length = (2 * M_PI * radius) / num_samples;
-
-		// Loop through each sample point on the vortex ring
-		for (int l = 0; l < num_samples; ++l) {
-			float theta = l / float(num_samples) * 2 * M_PI;
-			Vec p_sampled = radius * (cos(theta) * unit_x + sin(theta) * unit_y) + center;
-
-			Vec p_diff = pos - p_sampled;
-			float r = p_diff.length();
-
-			Vec w_vector = gamma * (-sin(theta) * unit_x + cos(theta) * unit_y);
-			float decay_factor = exp(-pow(r / delta, 3));
-
-			// Biot-Savart law contribution
-			velocity += curve_length * (-1 / (4 * M_PI * r * r * r)) * (1 - decay_factor) * p_diff.cross(w_vector);
-
-			// Smoke density update based on decay factor
-			smoke += (curve_length * decay_factor);
-		}
-	}
 
 	//set type, velocity, smoke
 	__device__ void setInitialCondition(HATileAccessor<Tile>& acc, HATileInfo<Tile>& info, const Coord& l_ijk)const;
