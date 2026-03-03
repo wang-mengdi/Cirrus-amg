6783b6555fd725f77b95a97455043cbd999a709c
Mengdi Wang <mengdi.wang@gatech.edu>
2026-03-03 03:02:31 -0500

    static water passed but something wrong with solver

--- tests/main.cpp
+++ tests/main.cpp
@@ -153,7 +153,7 @@ int main(int argc, char** argv) {
 	//SolverTests::TestSolverErrorSolid("sphere_solid", 2, 4);
 	//SolverTests::TestSolverErrorSolid("sphere_solid", 3, 5);
 	//SolverTests::TestSolverErrorSolid("sphere_solid", 4, 6);
-	//SolverTests::TestSolverErrorSolid("sphere_solid", 5, 7);
+	SolverTests::TestSolverErrorSolid("sphere_solid", 5, 7);
 	 
 	//SolverTests::TestRecoveryNew("sphere_solid", 2, 4);
 	//SolverTests::TestRecoveryNew("sphere_solid", 3, 5);
