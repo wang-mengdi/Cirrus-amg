02ba8d956169edac58dbcb19d713d7fb7b85fb76
Mengdi Wang <mengdi.wang@gatech.edu>
2026-03-06 03:51:09 -0500

    2-5 refinement works, try a larger all-neumann sphere flow

--- impulse/FluidParams.cu
+++ impulse/FluidParams.cu
@@ -40,18 +40,6 @@ __hostdev__ bool QueryBoundaryDirectionN1P1OnCoarseLevel(const HATileAccessor<Ti
 __hostdev__ bool QueryBoundaryDirectionN1P1OnCoarseLevel(const HATileAccessor<Tile>& acc, int coarse_chk_level, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk, int& boundary_axis, int& boundary_off) {
 	auto g_ijk = acc.localToGlobalCoord(info, l_ijk);
 	return QueryBoundaryDirectionN1P1OnCoarseLevel(acc, coarse_chk_level, info.mLevel, g_ijk, boundary_axis, boundary_off);
-
-
-	//coarse_chk_level = min(coarse_chk_level, info.mLevel);
-	//int level_diff = info.mLevel - coarse_chk_level;
-
-	//auto g_ijk = acc.localToGlobalCoord(info, l_ijk);
-
-
-	//g_ijk = Coord(g_ijk[0] >> level_diff, g_ijk[1] >> level_diff, g_ijk[2] >> level_diff);
-
-	////printf("level %d chk level %d diff %d g_ijk %d %d %d\n", info.mLevel, chk_level, level_diff, g_ijk[0], g_ijk[1], g_ijk[2]);
-	//return QueryBoundaryDirectionN1P1(acc, coarse_chk_level, g_ijk, boundary_axis, boundary_off);
 }
 
 FluidParams::FluidParams(json& j)
@@ -69,7 +57,6 @@ FluidParams::FluidParams(json& j)
 	mFlowMapStride = Json::Value<int>(j, "flowmap_stride", 5);
 	mCoarseLevel = Json::Value<int>(j, "coarse_level", 0);
 	mFineLevel = Json::Value<int>(j, "fine_level", 6);
-	//mRefineThreshold = Json::Value<T>(j, "refine_threshold", 200);
 	mGravity = Vec(0, 0, 0);
 	mGravity[2] = Json::Value<double>(j, "gravity", -9.8);
 	mParticleLife = Json::Value<T>(j, "particle_life", FLT_MAX);
@@ -83,20 +70,6 @@ __hostdev__ int FluidParams::initialLevelTarget(const HATileAccessor<Tile>& acc,
 	return mCoarseLevel;
 }
 
-//__hostdev__ void FluidParams::setInitialVelocity(HATileAccessor<Tile>& acc, HATileInfo<Tile>& info, const Coord& l_ijk) const {
-//	double current_time = 0.0;
-//
-//	if (mTestCase == MESHMOTION) {
-//		Vec initial_vel = Vec(0, 0, mesh_motion_inflow);
-//
-//		Tile& tile = info.tile();
-//		for (int axis : {0, 1, 2}) {
-//			tile(BufChnls::u + axis, l_ijk) = initial_vel[axis];
-//		}
-//		//int boundary_axis, boundary_off;
-//		//tile.type(l_ijk) = cellType(current_time, acc, info, l_ijk, boundary_axis, boundary_off);
-//	}
-//}
 
 __hostdev__ uint8_t FluidParams::wallCellType(const T current_time, const HATileAccessor<Tile>& acc, const int level, const Coord& g_ijk) const
 {
@@ -130,27 +103,6 @@ __hostdev__ uint8_t FluidParams::wallCellType(const T current_time, const HATile
 __hostdev__ uint8_t FluidParams::wallCellType(const T current_time, const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk) const {
 	auto g_ijk = acc.localToGlobalCoord(info, l_ijk);
 	return wallCellType(current_time, acc, info.mLevel, g_ijk);
-	//if (mTestCase == MESHMOTION) {
-	//	//uses 1 layer of solid walls on the coarse level
-
-	//	//z+ air
-	//	//other walls
-	//	bool is_neumann = false;
-	//	bool is_dirichlet = false;
-	//	int boundary_axis, boundary_off;
-
-	//	//int boundary_axis, boundary_off;
-	//	if (QueryBoundaryDirectionN1P1OnCoarseLevel(acc, mCoarseLevel, info, l_ijk, boundary_axis, boundary_off)) {
-	//		is_neumann = true;
-	//	}
-
-	//	if (is_neumann) return NEUMANN;
-	//	else if (is_dirichlet) return DIRICHLET;
-	//	return INTERIOR;
-	//}
-	//else {
-	//	return DIRICHLET;
-	//}
 }
 
 
@@ -160,54 +112,6 @@ __hostdev__ void FluidParams::setWallCellType(const T current_time, const HATile
 	auto& tile = info.tile();
 	tile.type(l_ijk) = wallCellType(current_time, acc, info, l_ijk);
 }
-
-//__hostdev__ void FluidParams::setVelocityBoundaryCondition(const T current_time, const HATileAccessor<Tile>& acc, const HATileInfo<Tile>& info, const nanovdb::Coord& l_ijk) const
-//{
-//	auto& tile = info.tile();
-//	if (mTestCase == MESHMOTION) {
-//		Vec vel(0, 0, mesh_motion_inflow);
-//
-//		for (int axis : {0, 1, 2}) {
-//			//first set all faces around NEUMANN to 0
-//			//there might be something wrong with a T-junction?
-//			{
-//				bool to_set = false;
-//				IterateFaceNeighborCellTypes(acc, info, l_ijk, axis, [&](const uint8_t type0, const uint8_t type1) {
-//					if ((type0 & NEUMANN) || (type1 & NEUMANN) || ((type0 & DIRICHLET) && (type1 & DIRICHLET))) {
-//						to_set = true;
-//					}
-//					});
-//
-//				//{
-//				//	//global coord (122, 142, 208) at level 5
-//				//	auto g_ijk = acc.localToGlobalCoord(info, l_ijk);
-//				//	if(info.mLevel == 5 && g_ijk == Coord(122, 142, 208)) {
-//				//		printf("g_ijk %d %d %d\n", g_ijk[0], g_ijk[1], g_ijk[2]);
-//				//		printf("type center %d\n", tile.type(l_ijk));
-//				//		printf("to_set: %d\n", to_set);
-//				//		IterateFaceNeighborCellTypes(acc, info, l_ijk, axis, [&](const uint8_t type0, const uint8_t type1) {
-//				//			printf("type neighbor %d %d\n", type0, type1);
-//				//			});
-//				//	}
-//				//}
-//
-//				if (to_set) {
-//					info.tile()(BufChnls::u + axis, l_ijk) = 0;
-//				}
-//			}
-//
-//			//then set the inflow velocity
-//			{
-//				auto nl_ijk = l_ijk; nl_ijk[axis] -= 1;
-//				if ((wallCellType(current_time, acc, info, l_ijk) & NEUMANN) || (wallCellType(current_time, acc, info, nl_ijk) & NEUMANN)) {
-//					tile(BufChnls::u + axis, l_ijk) = vel[axis];
-//				}
-//			}
-//		}
-//
-//	}
-//}
-
 
 __hostdev__ Eigen::Transform<T, 3, Eigen::Affine> FluidParams::meshToWorldTransform(const T current_time) const
 {
@@ -220,8 +124,8 @@ __hostdev__ Eigen::Transform<T, 3, Eigen::Affine> FluidParams::meshToWorldTransf
 		// t = 1 -> (0.5, 0.5, 0.3)
 		const T x = 0.5;
 		const T y = 0.5;
-		const T z0 = 0.5;// 0.8;
-		const T z1 = 0.5;// 0.3;
+		const T z0 = 0.8;// 0.8;
+		const T z1 = 0.3;// 0.3;
 		const T z = (1 - t) * z0 + t * z1;
 
 		Eigen::Transform<T, 3, Eigen::Affine> transform =
