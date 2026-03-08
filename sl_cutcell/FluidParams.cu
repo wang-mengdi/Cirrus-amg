9e4d0e0eb0eebe01e1ff9c4faaab3a4986cc54c3
Mengdi Wang <mengdi.wang@gatech.edu>
2026-03-08 03:11:08 -0400

    set 2-6 refine and output

--- sl_cutcell/FluidParams.cu
+++ sl_cutcell/FluidParams.cu
@@ -150,7 +150,7 @@ __hostdev__ Eigen::Transform<T, 3, Eigen::Affine> FluidParams::meshToWorldTransf
 		double jitter = 2.9e-4;//to avoid the mesh sdf exactly on the grid points, which can cause issues for some of the boundary condition implementations
 		const T x = 0.5 + jitter;
 		const T y = 0.5 + jitter;
-		const T z0 = 0.3 + jitter;// 0.8;
+		const T z0 = 0.8 + jitter;// 0.8;
 		const T z1 = 0.3 + jitter;// 0.3;
 		const T z = (1 - t) * z0 + t * z1;
 
