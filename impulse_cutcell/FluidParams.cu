57b4127a9db45dd20d2ed06682cdafbb3e21f836
Wang <mwang740@gatech.edu>
2026-03-09 15:52:36 -0400

    no jitter

--- impulse_cutcell/FluidParams.cu
+++ impulse_cutcell/FluidParams.cu
@@ -147,7 +147,8 @@ __hostdev__ Eigen::Transform<T, 3, Eigen::Affine> FluidParams::meshToWorldTransf
 		// Linear interpolation of center position over time:
 		// t = 0 -> (0.5, 0.5, 0.8)
 		// t = 1 -> (0.5, 0.5, 0.3)
-		double jitter = 2.9e-4;//to avoid the mesh sdf exactly on the grid points, which can cause issues for some of the boundary condition implementations
+		//double jitter = 2.9e-4;//to avoid the mesh sdf exactly on the grid points, which can cause issues for some of the boundary condition implementations
+		double jitter = 0;
 		const T x = 0.5 + jitter;
 		const T y = 0.5 + jitter;
 		const T z0 = 0.3 + jitter;// 0.8;
