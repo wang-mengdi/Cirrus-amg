177853c24cfe03405162ea0cf92c34cce8d095b8
Mengdi Wang <mengdi.wang@gatech.edu>
2026-04-08 16:59:53 -0400

    clean up repo

--- src/xmake.lua
+++ src/xmake.lua
@@ -6,10 +6,6 @@ includes("../common/xmake.lua")
 add_requires("eigen >=3.4.0")
 add_requires("cuda", {system = true})
 add_requires("vtk >=9.5.1", {configs = {cuda = true}})
---add_requireconfs("vtk.fast_float", {override = true, version = "3.10.1"})
---add_requires("polyscope =2.3")
---add_requireconfs("polyscope.glm", {override = true, version = "0.9.9+8"})
---add_requireconfs("polyscope.imgui", {override = true, version = "1.91.1"})
 add_requires("polyscope", {version = "2.5.0"})
 add_requireconfs("polyscope.glm", {override = true, version = "0.9.9+8"})
 
@@ -24,15 +20,7 @@ target("src")
     add_headerfiles("*.h","../ext/*.h")
     add_files("*.cpp","*.cu")
     add_includedirs(".", "../ext", {public=true})
-
-    -- if is_plat("windows") then
-    --     set_values("build.vcxproj.includes", "$(CUDA_PATH)/include")
-    -- end
-    -- add_includedirs("$(env CUDA_PATH)/include", "$(env CUDA_PATH)/include/cccl", {public = true})
-    -- if is_plat("windows") then
-    --     set_values("build.vcxproj.includes", "$(env CUDA_PATH)/include;$(env CUDA_PATH)/include/cccl")
-    -- end
-
+    
     add_cugencodes("native")
     add_cuflags("-extended-lambda --std=c++17 -lineinfo --allow-unsupported-compiler")
     add_cuflags("-rdc=true")
